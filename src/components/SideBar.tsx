8289bad2f6b94cb6982df7f1bf9d8c8f94e332af
Lucas Gabriel da Cruz Inada <86728737+lucasinada43@users.noreply.github.com>
2021-10-08 16:37:18 -0300

    Add files via upload

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,39 @@
+import '../styles/sidebar.scss'
+import {api} from '../services/api'
+import {useState, useEffect} from 'react'
+import {Button} from './Button'
+interface ArgSideBarProps{
+  selectedGenreId: number;
+  handleClickButton(id:number):void
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+export function SideBar({selectedGenreId, handleClickButton}:ArgSideBarProps) {
+  // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
+}
